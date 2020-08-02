a95b09b89cae8140d2c4da1cb808933945d1998a
Ex0dIa-dev <lucafranze3012@gmail.com>
2020-08-02 12:39:49 +0200

    Fixed Password Flag

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# go-sshclient
+### go-sshclient
+
+**Usage**:
+
+```go
+go run sshclient.go -u <username> -p -A <Ip Address> 
+Enter password: 
+```
+
+
+
+**Flag**:
+
+**-u** ---> define username
+
+**-p** ---> enter password
+
+**-A** ---> define ip address
+
+**-P** ---> define server port (default: 22)
+
--- sshclient.go
+++ sshclient.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type ServerAuth struct {
@@ -21,9 +23,22 @@ var auth ServerAuth
 
 func main() {
 
-	//Read server auth data
+	//flag for server auth data
+	flag.StringVar(&auth.Username, "u", "", "username")
+	flag.StringVar(&auth.IpAddress, "A", "", "server ip address")
+	flag.StringVar(&auth.Port, "P", "22", "server port")
+	passwordBool := flag.Bool("p", false, "use this flag for insert password")
 	flag.Parse()
-	
+
+	// if passwordBool == True enter password
+	if *passwordBool {
+		fmt.Print("Enter password: ")
+		tmp_passwd, err := terminal.ReadPassword(0)
+		CheckErr(err)
+		auth.Password = string(tmp_passwd)
+
+	}
+
 	//check if username and IpAddress are empty
 	if auth.Username == "" {
 		log.Println("[-]Please insert username.")
@@ -37,11 +52,11 @@ func main() {
 
 	conn_config := &ssh.ClientConfig{User: auth.Username, Auth: []ssh.AuthMethod{ssh.Password(auth.Password)}, HostKeyCallback: ssh.InsecureIgnoreHostKey()}
 	conn_addr := net.JoinHostPort(auth.IpAddress, auth.Port)
-	
+
 	//creating new connection
 	conn, err := ssh.Dial("tcp", conn_addr, conn_config)
 	CheckErr(err)
-	
+
 	//requesting a new session
 	session, err := conn.NewSession()
 	CheckErr(err)
@@ -67,15 +82,6 @@ func main() {
 	for {
 		io.Copy(stdin, os.Stdin)
 	}
-
-}
-
-func init() {
-
-	flag.StringVar(&auth.Username, "u", "", "username")
-	flag.StringVar(&auth.Password, "p", "", "password")
-	flag.StringVar(&auth.IpAddress, "A", "", "server ip address")
-	flag.StringVar(&auth.Port, "P", "22", "server port")
 
 }
 
